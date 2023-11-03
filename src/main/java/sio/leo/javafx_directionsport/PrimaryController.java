2b01b4b74f0c03872aa32a321a01012a4581e258
Corinne <cnovales@ac-nice.fr>
2023-11-03 17:30:14 +0100

    Version2 avec tableView et property String

--- src/main/java/sio/leo/javafx_directionsport/PrimaryController.java
+++ src/main/java/sio/leo/javafx_directionsport/PrimaryController.java
@@ -1,9 +1,13 @@
 package sio.leo.javafx_directionsport;
+//https://www.youtube.com/watch?v=7mpnDYcg_EA
+//***********************************
+//ajouter dans module-info : exports sio.leo.javafx_directionsport.modele;
 
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,11 +16,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import sio.leo.javafx_directionsport.modele.DAO_Utilisaleur;
-import sio.leo.javafx_directionsport.modele.Utilisateur;
-//https://www.youtube.com/watch?v=7mpnDYcg_EA
+import sio.leo.javafx_directionsport.modele.DAO_Utilisation;
+import sio.leo.javafx_directionsport.modele.Utilisation;
+
 public class PrimaryController implements Initializable{
      @FXML
     private ComboBox<String> cmbActivités;
@@ -31,12 +39,23 @@ public class PrimaryController implements Initializable{
 
     @FXML
     private Label lbl_user;
-        
+    
     @FXML
     private ComboBox<Integer> cmbQte;
-     ObservableList<Integer> lesQte=FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10);
+    ObservableList<Integer> lesQte=FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10);
 
-    DAO_Utilisaleur daoUser =new DAO_Utilisaleur();  ;
+    @FXML
+    private TableView<Utilisation> tbv_Passage;
+    
+    @FXML 
+    private TableColumn <Utilisation, String> col_Achat; 
+    @FXML
+    private TableColumn <Utilisation, String> col_Passage;
+    
+    public ObservableList<Utilisation> data= FXCollections.observableArrayList();
+    
+    DAO_Utilisaleur daoUser =new DAO_Utilisaleur();  
+    DAO_Utilisation daoPassage= new DAO_Utilisation();
     
 
     
@@ -51,9 +70,18 @@ public class PrimaryController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
       
-      cmbActivités.setItems(lesActivites); //TODO récupérer activites dans la base
-      cmbQte.setItems(lesQte);
-      
+         try {
+             cmbActivités.setItems(lesActivites); //TODO récupérer activites dans la base
+             cmbQte.setItems(lesQte);
+             
+             //remplissage tableView
+             col_Achat.setCellValueFactory(new PropertyValueFactory<>("ach_date"));
+             col_Passage.setCellValueFactory(new PropertyValueFactory<>("dateutilisation"));
+             
+             tbv_Passage.setItems(daoPassage.getLesUtilisations("ADM10"));
+         } catch (SQLException ex) {
+             Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
     @FXML
     public void cmbActiviteClic(ActionEvent levenement){
@@ -62,18 +90,18 @@ public class PrimaryController implements Initializable{
     }
     
     @FXML
-    private void valideLogin(MouseEvent event) throws SQLException {
-        Utilisateur user= daoUser.getUserById(txtId.getText());
-        System.out.println( user);
-        String login= user.getuti_nom();
-        lbl_user.setText(login);
-
+    private void valideLogin(MouseEvent event)  {
+            
+         try {
+             lbl_user.setText(daoUser.getUserById(txtId.getText()).getuti_nom());
+         } catch (SQLException ex) {
+             System.out.println("Probleme sur valideLogin()****************************************************");
+             Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+             
+         }
+        
     }
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-
+    
     
 
     
